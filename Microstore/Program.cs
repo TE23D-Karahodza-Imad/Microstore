4d74c51a1d2cbafeec9e75ce01a78202c51540bf
Imad Karahodza <imadahmed.karahodza@elev.ga.ntig.se>
2024-10-07 09:22:13 +0200

    DADAS

---
+++ Microstore/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlTypes;
+
+Console.WriteLine("Skriv in din ålder");
+string age = Console.ReadLine();
+
+int ageNum = 0;
+
+int.TryParse(age, out ageNum);
+
+bool success = int.TryParse(age, out ageNum);
+
+if(success == false)
+{
+    Console.WriteLine("No type a number ");
+}
+
+//-------------------------------------//
+Console.ReadLine();
+
